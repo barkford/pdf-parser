3effef28672590f44102665fdd8228693f45c2e1
jeff barker <jeff@crawfordbarker.com>
2015-10-01 00:16:06 -0400

    created lib, moved app there

---
+++ lib/pdf-scanner.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# coding: utf-8
+# Extract metadata only
+
+require 'rubygems'
+require 'pdf/reader'
+
+filename = File.dirname("/home/barkford/Downloads/2016jessupcompromis.pdf")
+
+PDF::Reader.open("/home/barkford/Downloads/2016jessupcompromis.pdf") do |reader|
+  puts reader.info.inspect
+  puts reader.metadata.inspect
+end
